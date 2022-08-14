0a84889bb16bacf117e0d7739bf63bea8fbd1fdb
Mayank Rawat <mayank.rawat6802@gmail.com>
2022-08-14 14:51:45 +0530

    Update dpStriver.cpp

--- DP/dpStriver.cpp
+++ DP/dpStriver.cpp
@@ -220,3 +220,175 @@ int cutRod3(vector<int> &p, int n)
     }
     return prev[n];
 }
+int maxMerit(vector<vector<int>> v, int x, int d) {
+    int n = v.size();
+    // if(n <= 0) {
+    //     return 0;
+    // }
+    if(d >= n) {
+        return 0;
+    }
+    int ans = 0;
+    for(int i = 0; i < 3; i++) {
+        if(x!=i) {
+            int pick = v[d][i] + maxMerit(v, i, d + 1);
+            ans = max(ans, pick);
+        }
+    }
+    return ans;
+}
+int maxMerit1(vector<vector<int>> v, int x, int d) {
+    int n = v.size();
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    if(d >= n) {
+        return 0;
+    }
+    if(dp[d][x]!= -1) {
+        return dp[d][x];
+    }
+    int ans = 0;
+    for(int i = 0; i < 3; i++) {
+        if(x != i) {
+            int pick = v[d][i] + maxMerit1(v, i, d + 1);
+            ans = max(ans, pick);
+        }
+    }
+    return dp[d][x] = ans;
+}
+int maxMerit2(vector<vector<int>> v, int x, int d) {
+    int n = v.size();
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    dp[0][0] = max(v[0][1], v[0][2]);
+    dp[0][1] = max(v[0][0], v[0][2]);
+    dp[0][2] = max(v[0][0], v[0][1]);
+    dp[0][3] = max(max(v[0][0], v[0][1]), v[0][2]);
+    for(int i = 1; i < n; i++) {
+        int temp =0;
+        for(int j = 0; j < 4; j++) {
+            for(int k = 0; k < 3; k++) {
+                if(k != j) {
+                    temp =  max(v[i][k] + dp[i - 1][k], temp);
+                }
+            }
+            dp[i][j] = temp;
+        }
+    }
+    return dp[n - 1][3];
+}
+int maxMerit3(vector<vector<int>> v, int x, int d) {
+    int n = v.size();
+    vector<int> dp(4, 0);
+    dp[0] = max(v[0][1], v[0][2]);
+    dp[1] = max(v[0][0], v[0][2]);
+    dp[2] = max(v[0][0], v[0][1]);
+    dp[3] = max(max(v[0][0], v[0][1]), v[0][2]);
+    for(int i = 1; i < n; i++) {
+        int temp =0;
+        vector<int> t(4, 0);
+        for(int j = 0; j < 4; j++) {
+            for(int k = 0; k < 3; k++) {
+                if(k != j) {
+                    temp =  max(v[i][k] + dp[k], temp);
+                }
+            }
+            t[j] = temp;
+        }
+        dp = t;
+    }
+    return dp[3];
+}
+int minimumDifference(vector<int>& v) {
+    int n = v.size();
+    int t = accumulate(v.begin(), v.end(), 0);
+    vector<vector<bool>> dp(n, vector<bool>(t + 1, false));
+    for(int i = 0; i < n; i++) {
+        dp[i][0] =true;
+    }
+    if(v[0] <= t && v[0] >= 0)  dp[0][v[0]] = true;
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j <= t; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if(j - v[i] >= 0)   dp[i][j] = dp[i][j] || dp[i][j - v[i]];
+        }
+    }
+    int mini = 1e9;
+    for(int i = 0; i <= t; i++) {
+        if(dp[n - 1][i] == true) {
+            int s1 = i;
+            int s2 = t - s1;
+            mini = min(abs(s1 - s2), mini);
+        }
+    }
+    return mini;        
+}
+int countSubsets(vector<int>& v, int i, int s) {
+    if(s < 0) {
+        return 0;
+    }
+    if(s == 0 && i >= 0) {
+        return 1;
+    }
+    if(i == 0) {
+        return v[i] == s;
+    }
+    int pick = 0, notPick = 0;
+    if(v[i] <= s)    pick = countSubsets(v, i - 1, s - v[i]);
+    notPick = countSubsets(v, i - 1, s);
+    return pick + notPick;
+}
+int fcs(vector<int>& v, int i, int j, vector<vector<int>>& dp) {
+    if(j < 0) {
+        return 0;
+    }
+    if(j == 0 && i >= 0) {
+        return 1;
+    }
+    if(i == 0) {
+        return v[i] == j;
+    }
+    if(dp[i][j] != -1) return dp[i][j];
+    int pick = 0, notPick = 0;
+    if(v[i] <= j)    pick = fcs(v, i - 1, j - v[i], dp);
+    notPick = fcs(v, i - 1, j, dp);
+    return dp[i][j] = pick + notPick;
+}
+int countSubsets1(vector<int>& v, int s) {
+    int n = v.size();
+    vector<vector<int>> dp(n, vector<int>(s + 1, -1));
+    return fcs(v, s, n - 1, dp);
+}
+int countSubsets2(vector<int>& v, int s) {
+    int n = v.size();
+    vector<vector<int>> dp(n, vector<int>(s + 1, 0));
+    for(int i = 0; i < n; i++) {
+       dp[i][0] = 1;
+    }
+    if(v[0] <= s) dp[0][v[0]] = 1;
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j <= s; j++) {
+            int pick = 0, notPick = 0;
+            if(v[i] <= j)    pick = dp[i - 1][j - v[i]];
+            notPick = dp[i - 1][j];
+            dp[i][j] = pick + notPick;
+        }
+    }
+    return dp[n - 1][s];
+}
+int countSubsets3(vector<int>& v, int s) {
+    int n = v.size();
+    vector<int> prev(s + 1, 0);
+    prev[0] = 1;
+    if(v[0] <= s) prev[v[0]] = 1;
+    for(int i = 1; i < n; i++) {
+        vector<int> curr(s + 1, 0);
+        curr[0] = 1;
+        for(int j = 1; j <= s; j++) {
+            int pick = 0, notPick = 0;
+            if(v[i] <= j)    pick = prev[j - v[i]];
+            notPick = prev[j];
+            curr[j] = pick + notPick;
+        }
+        prev = curr;
+    }
+    return prev[s];
+}
