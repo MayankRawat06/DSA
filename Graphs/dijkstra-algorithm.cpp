61ac6320a491d2643e679c1c14d1f0072abd0303
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-04-16 17:24:56 +0530

    add dijkstra algorithm using set

--- Graphs/dijkstra-algorithm.cpp
+++ Graphs/dijkstra-algorithm.cpp
@@ -1,12 +1,12 @@
 class Solution
 {
-    public:
-    //Function to find the shortest distance of all the vertices
+	public:
+	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
     vector <int> dijkstra(int n, vector<vector<int>> adj[], int s)
     {
-        // using priority queue -- min heap
-        vector<int> dis(n, 1e9);
+        // using priority queue -- max heap 
+        vector<int> dis(n, INT_MAX);
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, s});
         dis[s] = 0;
@@ -20,6 +20,37 @@ class Solution
                 if(dis[neighbor] > d + far) {
                     dis[neighbor] = d + far;
                     pq.push({dis[neighbor], neighbor});
+                }
+            }
+        }
+        return dis;
+    }
+};
+
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int n, vector<vector<int>> adj[], int s)
+    {
+        // using set -- erase already existing paths
+        vector<int> dis(n, 1e9);
+        set<pair<int, int>> set;
+        set.insert({0, s});
+        dis[s] = 0;
+        while(!set.empty()) {
+            auto it = *set.begin();
+            int d = it.first;
+            int node = it.second;
+            set.erase({d, node});
+            for(auto x : adj[node]) {
+                int neighbor = x[0];
+                int wt = x[1];
+                if(wt + d < dis[neighbor]) {
+                    if(dis[neighbor] != 1e9) set.erase({dis[neighbor], neighbor});
+                    dis[neighbor] = wt + d;
+                    set.insert({dis[neighbor], neighbor});
                 }
             }
         }
