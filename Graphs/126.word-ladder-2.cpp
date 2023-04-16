e9b67225a34d1eaa8cf5d3bc67e7bd99aa1440bd
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-04-16 12:16:11 +0530

    update word ladder 2

--- Graphs/126.word-ladder-2.cpp
+++ Graphs/126.word-ladder-2.cpp
@@ -1,3 +1,5 @@
+
+
 // gives TLE 
 class Solution {
 public:
@@ -37,3 +39,63 @@ public:
         }
         return res;
     }
+
+
+// optimized
+
+class Solution {
+    void dfs(string word, vector<string>& path, vector<vector<string>>& res, unordered_map<string, int>& map) {
+        int level = map[word];
+        if(level == 1) {
+            res.push_back(path);
+            return;
+        }
+        for(int i = 0; i < word.size(); i++) {
+            char og = word[i];
+            for(char c = 'a'; c <= 'z'; c++) {
+                word[i] = c;
+                if(map.find(word) != map.end() && map[word] < level) {
+                    path.push_back(word);
+                    dfs(word, path, res, map);
+                    path.pop_back();
+                }
+            }
+            word[i] = og;
+        }
+    }
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, int> map;
+        vector<vector<string>> res;
+        unordered_set<string> set(wordList.begin(), wordList.end());
+        queue<string> q;
+        q.push(beginWord);
+        map[beginWord] = 1;
+        int level = 1;
+        set.erase(beginWord);
+        while(!q.empty()) {
+            string word = q.front(); q.pop();
+            level = map[word];
+            if(word == endWord) break;
+            for(int i = 0; i < word.size(); i++) {
+                char og = word[i];
+                for(char c = 'a'; c <= 'z'; c++) {
+                    word[i] = c;
+                    if(set.find(word) != set.end()) {
+                        q.push(word);
+                        map[word] = level + 1;
+                        set.erase(word);
+                    }
+                }
+                word[i] = og;
+            }
+        }
+        if(map.find(endWord) != map.end()) {
+            vector<string> path;
+            path.push_back(endWord);
+            dfs(endWord, path, res, map);
+            for(vector<string>& v : res) reverse(v.begin(), v.end());
+        }
+        return res;
+    }
+};
