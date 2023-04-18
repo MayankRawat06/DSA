9adff6f46c0b4dbd6f5d6ceb0fb1df3e03aa7b66
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-04-18 17:30:30 +0530

    update binary maze

--- Graphs/1091.shortest-path-in-binary-matrix.cpp
+++ Graphs/1091.shortest-path-in-binary-matrix.cpp
@@ -1,3 +1,36 @@
+// using queue -- better - eliminate log n of complexity
+class Solution {
+    vector<int> dx{-1, -1, -1, 0, 0, 1, 1, 1}, dy{-1, 0, 1, -1, 1, -1, 0, 1};
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1) return -1;
+        vector<vector<int>> dis(n, vector<int>(n, 1e9));
+        queue<pair<int, pair<int, int>>> q;
+        dis[0][0] = 1;
+        q.push({1, {0, 0}});
+        while(!q.empty()) {
+            auto it = q.front();
+            int d = it.first;
+            int row = it.second.first, col = it.second.second;
+            q.pop();
+            for(int i = 0; i < 8; i++) {
+                int delx = dx[i], dely = dy[i];
+                int r = row + delx, c = col + dely;
+                if(r >= 0 && r < n && c >= 0 && c < n && grid[r][c] == 0) {
+                    if(d + 1 < dis[r][c]) {
+                        dis[r][c] = d + 1;
+                        q.push({dis[r][c], {r, c}});
+                    }
+                }
+            }
+        }
+        return dis[n - 1][n - 1] == 1e9 ? -1 : dis[n - 1][n - 1];
+    }
+};
+
+
+// using set
 class Solution {
     vector<int> dx{-1, -1, -1, 0, 0, 1, 1, 1}, dy{-1, 0, 1, -1, 1, -1, 0, 1};
 public:
