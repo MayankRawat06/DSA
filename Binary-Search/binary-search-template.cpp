b781f88d720af33f6e9f52be71d9463cbeb08d61
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-06-18 23:42:58 +0530

    add rule

--- Binary-Search/binary-search-template.cpp
+++ Binary-Search/binary-search-template.cpp
@@ -6,3 +6,9 @@ while (left < right) {
         left = mid + 1;
 }
 return left;
+/*
+ When using mid and mid-1 put (l + r + 1) // 2 -- choosing maximum 
+ When using mid and mid + 1 put (l + r)//2 or l + (r - l) / 2 -- choosing minimum
+
+This is because we want to push the boundaries in both cases. When using mid and mid +1 we are pushing the lower bondary with mid+1 so to also push the higher boundary we use by default a lower mid (aka l+r//2). And vice versa.
+*/
