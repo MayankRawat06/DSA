32ad7226ffc1d12d93242154ae56a9275619dda8
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-07-24 11:42:27 +0530

    add LC 50

---
+++ Recursion/50.pow_x,n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    double f(double x, long n) {
+        if(n == 0 || x == 1) return 1;
+        if(n < 0) return f(1 / x, -n);
+        if(n % 2 == 0) return f(x * x, n / 2);
+        else return x * f(x * x, (n - 1) / 2);
+    }
+public:
+    double myPow(double x, int n) {
+        return f(x, n);
+    }
+};
