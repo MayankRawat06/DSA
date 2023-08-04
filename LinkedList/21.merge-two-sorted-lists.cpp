82c28e4bac7980a7fed3d086dc9503dc17cc1cd8
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-08-05 00:00:32 +0530

    add LC 21

---
+++ LinkedList/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* h1, ListNode* h2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        while(h1 && h2) {
+            if(h1 -> val < h2 -> val) {
+                tail -> next = h1;
+                h1 = h1 -> next;
+            }
+            else {
+                tail -> next = h2;
+                h2 = h2 -> next;
+            }
+            tail = tail -> next;
+        }
+        while(h1) {
+            tail -> next = h1;
+            h1 = h1 -> next;
+            tail = tail -> next;
+        }
+         while(h2) {
+            tail -> next = h2;
+            h2 = h2 -> next;
+            tail = tail -> next;
+        }
+        return dummy -> next;
+    }
+};
