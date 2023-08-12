a62e025a8f050a70dbe3646a91f9f27f993b8123
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-08-12 16:10:16 +0530

    update allocate books

--- Binary-Search/allocate-minimum-number-of-pages.cpp
+++ Binary-Search/allocate-minimum-number-of-pages.cpp
@@ -5,19 +5,21 @@ class Solution
     int n;
     public:
     //Function to find minimum number of pages.
-    int students(int v[], int mid) {
+    bool students(int v[], int mid, int k) {
         int count = 0;
         long long sum = 0;
         for(int i = 0; i < n; i++) {
+            if(v[i] > mid) return false;
             if(sum + v[i] <= mid) sum += v[i];
             else {
                 count++;
                 sum = v[i];
             }
         }
-        // cout << sum << endl;
-        if(sum <= mid) return count + 1;
-        else return 1e9;
+        if(sum <= mid) count++;
+        else return false;
+        if(count <= k) return true;
+        else return false;
     }
     int findPages(int v[], int N, int k) 
     {
@@ -33,7 +35,7 @@ class Solution
         long long low = mini, high = sum;
         while(low < high) {
             long long mid = low + (high - low) / 2;
-            if(students(v, mid) <= k) high = mid; // students less or equal to mid -- decrease the pages
+            if(students(v, mid, k)) high = mid; // students less or equal to mid -- decrease the pages
             else low = mid + 1; // students more than mid -- increase the pages
         }
         return low;
