029e68ece754833de37e1f85e262fbc06d427da7
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-08-22 23:30:19 +0530

    add gfg qs

--- greedy/minimize-max-distance-to-gas-station.cpp
+++ greedy/minimize-max-distance-to-gas-station.cpp
@@ -1,1 +1,21 @@
-Minimize Max Distance to Gas Station
+class Solution {
+  public:
+    double findSmallestMaxDist(vector<int> &v, int k){
+      // Code here
+        int n = v.size();
+        priority_queue<pair<double, int>> pq;
+        vector<int> freq(n - 1, 0);
+        for(int i = 0; i < n - 1; i++) 
+            pq.push({v[i + 1] - v[i], i});
+        while(k--) {
+            double maxi = pq.top().first;
+            int index = pq.top().second;
+            pq.pop();
+            double initial = v[index + 1] - v[index];
+            freq[index]++;
+            double newlen = initial / (freq[index] + 1);
+            pq.push({newlen, index});
+        }
+        return pq.top().first;
+    }
+};
