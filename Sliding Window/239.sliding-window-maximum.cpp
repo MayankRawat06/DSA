59a4525fc3b3cc8f844e8b2a35c125e3e9468a5d
Mayank Rawat <mayank.rawat6802@gmail.com>
2024-10-08 22:47:42 +0530

    update LC 239

--- Sliding Window/239.sliding-window-maximum.cpp
+++ Sliding Window/239.sliding-window-maximum.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& v, int k) {
-        int n = v.size();
-        deque<int> q;
+        // monotonic stack
+        // -- front - largest element but always the smaller index  
+        // -- back - smallest element but could be largest index
         vector<int> res;
+        deque<int> q; // indexes
+        int n = v.size();
         for(int i = 0; i < n; i++) {
+            // check out of bound from front 
             while(!q.empty() && q.front() <= i - k) q.pop_front();
+            // remove any elements from queue that are smaller than current index element in array - from back
             while(!q.empty() && v[q.back()] <= v[i]) q.pop_back();
             q.push_back(i);
             if(i >= k - 1) res.push_back(v[q.front()]);
