36ac2e842a929ed02886e694d9108fed0d47e0ad
Mayank Rawat <mayank.rawat6802@gmail.com>
2024-10-29 16:49:52 +0530

    add LC 146

--- system-design/146.LRU-cache.cpp
+++ system-design/146.LRU-cache.cpp
@@ -47,3 +47,84 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+
+// alternative solution
+
+class LRUCache {
+    class Node {
+public:
+    int key;
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int key, int val) {
+        this -> key = key;
+        this -> val = val;
+        this -> prev = NULL;
+        this -> next = NULL;
+    }
+};
+    int cap;
+    unordered_map<int, Node*> map;
+    Node *head;
+    Node *tail;
+    void insertNode(int key, int val) {
+        Node *node = new Node(key, val);
+        Node *temp = head -> next;
+        temp -> prev = node;
+        node -> next = temp;
+        node -> prev = head;
+        head -> next = node;
+    }
+    void deleteNode(Node *node) {
+        Node *prev = node -> prev;
+        Node *next = node -> next;
+        prev -> next = next;
+        next -> prev = prev;
+        delete node;
+    }
+public:
+    LRUCache(int capacity) {
+        this -> cap = capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head -> next = tail;
+        tail -> prev = head;
+    }
+    
+    int get(int key) {
+        if(map.find(key) != map.end()) {
+            Node *node = map[key];
+            int val = node -> val;
+            deleteNode(node);
+            insertNode(key, val);
+            map[key] = head -> next;
+            return val;
+        }
+        else return -1;
+    }
+    
+    void put(int key, int value) {
+        if(map.find(key) != map.end()) {
+            Node *node = map[key];
+            deleteNode(node);
+        } else {
+            if(map.size() >= cap) {
+                Node* leastRecent = tail -> prev;
+                int keyToDelete = leastRecent -> key;
+                map.erase(keyToDelete);
+                deleteNode(leastRecent);
+            }
+        }
+        insertNode(key, value);
+        map[key] = head -> next;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
