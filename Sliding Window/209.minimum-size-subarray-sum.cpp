69a8c8705bd6cf276f3f388487a049104c1a02f8
Mayank Rawat <mayank.rawat6802@gmail.com>
2024-11-17 17:06:33 +0530

    update LC 209

--- Sliding Window/209.minimum-size-subarray-sum.cpp
+++ Sliding Window/209.minimum-size-subarray-sum.cpp
@@ -1,3 +1,8 @@
+
+// Kadane's Algorithm
+// TC = O(n)
+// SC = O(1)
+
 class Solution {
 public:
     int minSubArrayLen(int k, vector<int>& v) {
@@ -11,5 +16,39 @@ public:
             }
         }
         return mini > 1e9 ? 0 : mini;
+    }
+};
+
+
+// Prefix sum + binary Search
+// TC = O(nlogn)
+// SC = O(n)
+
+class Solution {
+public:
+    int minSubArrayLen(int k, vector<int>& v) {
+        map<int, int> map;
+        int n = v.size();
+        map[0] = -1;
+        int prev = 0;
+        int res = 1e9;
+        for(int i = 0; i < n; i++) {
+            int pref = prev + v[i];
+            int x = pref - k;
+            map[pref] = i;
+            auto it = map.lower_bound(x);
+            if(it != map.end()) {
+                if(it -> first == x) {
+                    res = min(res, i - it -> second);
+                } else {
+                    if(it -> first != 0) {
+                        it--;
+                        res = min(res, i - it -> second);
+                    }
+                }
+            }
+            prev += v[i];
+        }
+        return res == 1e9 ? 0 : res;
     }
 };
